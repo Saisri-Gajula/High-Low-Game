63d55c471d7dcafc1de73f9309fa627b60cfa309
Saisri-Gajula <140395758+Saisri-Gajula@users.noreply.github.com>
2023-08-21 17:36:53 +0530

    Added High Low Game

---
+++ src/main/java/com/learning/hello/controller/HighLowController.java
@@ -0,0 +1,23 @@
+package com.learning.hello.controller;
+import java.util.Random;
+
+public class HighLowController {
+  private int correct;
+  private int guess;
+  
+  public void setGuess(int guess) {
+    this.guess = guess;
+  }
+  
+  public void reset() {
+    this.correct = new Random().nextInt(0, 100);
+  }
+  
+  public int judge() {
+    return Integer.compare(guess, correct);
+  }
+  
+  public String feedback() {
+    return judge() == 0 ? "Correct!" : judge() == 1 ? "Guess lower" : "guess higher";
+  }
+}
